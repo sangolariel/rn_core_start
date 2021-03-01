220bf38a7e1934bf754da97393194926669f50c4
sangnh <sangnh@futurify.vn>
2021-03-01 10:19:53 +0700

    firebase

--- src/configs/firebase/index.js
+++ src/configs/firebase/index.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyBXGzvvSvac6UzMjdJkiOrhm9L-CPiw51Y",
-  authDomain: "rn-mrsimple-thepolo.firebaseapp.com",
-  projectId: "rn-mrsimple-thepolo",
-  storageBucket: "rn-mrsimple-thepolo.appspot.com",
+  authDomain: "xxx.firebaseapp.com",
+  projectId: "xxx",
+  storageBucket: "xxx.appspot.com",
   messagingSenderId: "534442807360",
   appId: "1:534442807360:web:9231bece2cca698fa0cfa5",
   measurementId: "G-R8PPCXEZY3"
